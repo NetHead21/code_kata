177d56ad05392155f9a22187caf06a4a2a77b783
nethead21 <junivensaavedra@gmail.com>
2026-05-30 23:35:24 +0800

    add: import additional functions for Kata8: Conflicting Objectives tests

--- test_kata_8_conflicting_objectives.py
+++ test_kata_8_conflicting_objectives.py
@@ -4,3 +4,13 @@ import time
 from pathlib import Path
 
 import pytest
+
+from kata_7_conflicting_objectives import (
+    _iter_splits,
+    find_compound_words_extendible,
+    find_compound_words_fast,
+    find_compound_words_readable,
+    load_words,
+    normalise_extendible,
+    normalise_readable,
+)
