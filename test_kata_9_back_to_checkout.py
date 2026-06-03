e6d4b114d3a30f875565c0837b3bc6336d72d703
nethead21 <junivensaavedra@gmail.com>
2026-06-04 01:07:28 +0800

    add: implement initial test setup for Kata09: Back to the Checkout

---
+++ test_kata_9_back_to_checkout.py
@@ -0,0 +1,14 @@
+"""Tests for Kata09: Back to the Checkout."""
+
+import pytest
+
+from kata_9_back_to_checkout import (
+    BuyNGetMFree,
+    CheckOut,
+    PercentageDiscount,
+    PricingRule,
+    RULES,
+    SpecialPrice,
+    UnitPrice,
+    price,
+)
