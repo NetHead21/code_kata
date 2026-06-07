aecbbf15e80e6e384ad4f1ec52d98c8c97d7941d
nethead21 <junivensaavedra@gmail.com>
2026-06-07 23:46:41 +0800

    add: implement test for SpecialPrice with special price without quantity validation

--- test_kata_9_back_to_checkout.py
+++ test_kata_9_back_to_checkout.py
@@ -165,3 +165,7 @@ class TestSpecialPrice:
     def test_only_special_qty_without_price_raises(self):
         with pytest.raises(ValueError):
             SpecialPrice(unit_price=50, special_qty=3)
+
+    def test_only_special_price_without_qty_raises(self):
+        with pytest.raises(ValueError):
+            SpecialPrice(unit_price=50, special_price=130)
