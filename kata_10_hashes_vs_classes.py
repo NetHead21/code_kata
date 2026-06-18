eeeefecff7c34d126d42f7cc8b00ee77a46b4716
nethead21 <junivensaavedra@gmail.com>
2026-06-18 23:28:40 +0800

    add: implement export method in ClassBasedExporter to generate fully populated ExportRecords

--- kata_10_hashes_vs_classes.py
+++ kata_10_hashes_vs_classes.py
@@ -115,3 +115,6 @@ class ClassBasedExporter:
         self._customers = {c.customer_id: c for c in customers}
         self._orders = orders
         self._shipping = {s.order_id: s for s in shipping_details}
+
+    def export(self) -> list[ExportRecord]:
+        """Build and return one ExportRecord per order, fully populated."""
